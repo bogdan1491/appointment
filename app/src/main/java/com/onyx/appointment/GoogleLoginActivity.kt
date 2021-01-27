8994e4157866dea3a6c1426ae4f82482b774adcc
Bogdan <bogdan.onica@p3-group.com>
2021-01-27 10:24:36 +0200

    Add GoogleLoginActivity.

---
+++ app/src/main/java/com/onyx/appointment/GoogleLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.onyx.appointment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GoogleLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- app/src/main/java/com/onyx/appointment/LoginActivity.kt
+++
@@ -1,4 +0,0 @@
-package com.onyx.appointment
-
-class LoginActivity {
-}
